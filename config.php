5805233c8f22016aaea41505d200136bbfe2f4f8
trungnq1 <trungnq1@smartosc.com>
2015-03-11 14:34:58 +0700

    upload code the fist

---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/nhuaaptec/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/nhuaaptec/');
+
+// DIR
+define('DIR_APPLICATION', 'D:\Xampp\htdocs\nhuaaptec/catalog/');
+define('DIR_SYSTEM', 'D:\Xampp\htdocs\nhuaaptec/system/');
+define('DIR_DATABASE', 'D:\Xampp\htdocs\nhuaaptec/system/database/');
+define('DIR_LANGUAGE', 'D:\Xampp\htdocs\nhuaaptec/catalog/language/');
+define('DIR_TEMPLATE', 'D:\Xampp\htdocs\nhuaaptec/catalog/view/theme/');
+define('DIR_CONFIG', 'D:\Xampp\htdocs\nhuaaptec/system/config/');
+define('DIR_IMAGE', 'D:\Xampp\htdocs\nhuaaptec/image/');
+define('DIR_CACHE', 'D:\Xampp\htdocs\nhuaaptec/system/cache/');
+define('DIR_DOWNLOAD', 'D:\Xampp\htdocs\nhuaaptec/download/');
+define('DIR_LOGS', 'D:\Xampp\htdocs\nhuaaptec/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'nhuaaptec');
+define('DB_PREFIX', 'oc_');
+?>
