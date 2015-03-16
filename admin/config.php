035e4adc16a3083d26f38fa35451c76d4d9eb018
quangtrung <trungqt2005@gmail.com>
2015-03-16 03:09:01 +0000

    admin config

--- admin/config.php
+++ admin/config.php
@@ -1,29 +1,29 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://localhost/nhuaaptec/admin/');
-define('HTTP_CATALOG', 'http://localhost/nhuaaptec/');
+define('HTTP_SERVER', 'http://nhuaaptec-quartermain-1.c9.io/admin/');
+define('HTTP_CATALOG', 'http://nhuaaptec-quartermain-1.c9.io/');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://localhost/nhuaaptec/admin/');
-define('HTTPS_CATALOG', 'http://localhost/nhuaaptec/');
+define('HTTPS_SERVER', 'http://nhuaaptec-quartermain-1.c9.io/admin/');
+define('HTTPS_CATALOG', 'http://nhuaaptec-quartermain-1.c9.io/');
 
 // DIR
-define('DIR_APPLICATION', 'D:\Xampp\htdocs\nhuaaptec/admin/');
-define('DIR_SYSTEM', 'D:\Xampp\htdocs\nhuaaptec/system/');
-define('DIR_DATABASE', 'D:\Xampp\htdocs\nhuaaptec/system/database/');
-define('DIR_LANGUAGE', 'D:\Xampp\htdocs\nhuaaptec/admin/language/');
-define('DIR_TEMPLATE', 'D:\Xampp\htdocs\nhuaaptec/admin/view/template/');
-define('DIR_CONFIG', 'D:\Xampp\htdocs\nhuaaptec/system/config/');
-define('DIR_IMAGE', 'D:\Xampp\htdocs\nhuaaptec/image/');
-define('DIR_CACHE', 'D:\Xampp\htdocs\nhuaaptec/system/cache/');
-define('DIR_DOWNLOAD', 'D:\Xampp\htdocs\nhuaaptec/download/');
-define('DIR_LOGS', 'D:\Xampp\htdocs\nhuaaptec/system/logs/');
-define('DIR_CATALOG', 'D:\Xampp\htdocs\nhuaaptec/catalog/');
+define('DIR_APPLICATION', '/home/ubuntu/workspace/admin/');
+define('DIR_SYSTEM', '/home/ubuntu/workspace/system/');
+define('DIR_DATABASE', '/home/ubuntu/workspace/system/database/');
+define('DIR_LANGUAGE', '/home/ubuntu/workspace/admin/language/');
+define('DIR_TEMPLATE', '/home/ubuntu/workspace/admin/view/template/');
+define('DIR_CONFIG', '/home/ubuntu/workspace/system/config/');
+define('DIR_IMAGE', '/home/ubuntu/workspace/image/');
+define('DIR_CACHE', '/home/ubuntu/workspace/system/cache/');
+define('DIR_DOWNLOAD', '/home/ubuntu/workspace/download/');
+define('DIR_LOGS', '/home/ubuntu/workspace/system/logs/');
+define('DIR_CATALOG', '/home/ubuntu/workspace/catalog/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
-define('DB_HOSTNAME', 'localhost');
-define('DB_USERNAME', 'root');
+define('DB_HOSTNAME', getenv('IP'));
+define('DB_USERNAME', getenv('C9_USER'));
 define('DB_PASSWORD', '');
 define('DB_DATABASE', 'nhuaaptec');
 define('DB_PREFIX', 'oc_');
